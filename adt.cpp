a61bf17b5dd781ce0bd362fabbe042dcbf2432e3
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-11 01:14:17 -0400

    Update adt.cpp
    Haven't tested it

--- adt.cpp
+++ adt.cpp
@@ -1,4 +1,22 @@
 // FILE: adt.cpp 
 // Coder: J Yong
 // Purpose: this can be deleted 
-#include 
+#include "adt.h"
+
+//id arr burst priority
+bool read_File (struct Queue q) {
+  int i = 0;
+  ifstream inFile("input"); 
+  if (!inFile.is_open()) {
+    return false;
+  }
+  while (!inFile.eof()) { //fig how to look for unfinished lines?
+     inFile >> q[i].p_id;
+     inFile >> q[i].arrival_time;
+     inFile >> q[i].total_CPU_burst;
+     inFile >> q[i].priority; 
+     i++;
+  }
+  return true;
+  inFile.close();
+}
