5b461b5c74203f9d9c282b2dbc01d370cbbbc8d4
DanAossey <92216534+DanAossey@users.noreply.github.com>
2021-10-13 13:09:39 -0400

    Update roundRobin.h

--- roundRobin.h
+++ roundRobin.h
@@ -1,10 +1,22 @@
-// FILE: roundRobin.h
+// FILE: redRobin.h
 // Coder:
 // Purpose:
 //
-#ifndef ROUNDROBIN_H_INCLUDED
-#define ROUNDROBIN_H_INCLUDED
+#ifndef REDROBIN_H_INCLUDED
+#define REDROBIN_H_INCLUDED
 
 #include "adt.h"
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <vector>
+using namespace std;
+
+
+int roundRobin(Queue colleciton, int quantum, int numberOfProcesses);
+bool checkFinished (Queue collection, int numberOfProcesses);
 
 #endif 
