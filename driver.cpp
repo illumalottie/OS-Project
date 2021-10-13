4d5539e8d48d81f9ebfa0ec1a38d4d65e34d89c3
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-12 22:02:47 -0400

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,18 @@
 // FILE: driver.cpp
 // Coders: Group 3 - Joey Yong, Lottie Maynard, and Dan
-// Purpose:
+// Purpose: Simulates the scheduling for a CPU
+//  INPUT: input file "input.in" 
+//         p_id  arrival_time  total_CPU_burst  Priority
+//         
+//         --type {FCFS|SJF|Priority|RR}
+//         --type preemptive (not is the default)
+//         --quanta#(10 is the default)
+//         --file NAME (sched.in is the default)
+//         --verbose (not is the default) for the extra info 
+//
+//  OUTPUT: average wait time for each process
+//
+//  Side-Quests: detect unfinished lines
 //
 #include <iostream>
 #include "fcfs.h"
@@ -11,17 +23,17 @@
 
 using namespace std;
 
-int main(void) {
-Queue g[100]; //maybe do the lexical definition he mentioned? thoughts? - lottie
+int main(int argc, char **argv) {
+  Queue q[100]; //maybe do the lexical definition he mentioned? thoughts? - lottie
 
-read_File(g);
+  read_File(q);
 
-//i can pass in an Queue array and it brings back all the info from the file
-//below the couts are just tests to make sure it returns the right numbers - lottie
+  //i can pass in an Queue array and it brings back all the info from the file
+  //below the couts are just tests to make sure it returns the right numbers - lottie
 
 
 
-cout << g[0].p_id << endl;
-cout << g[1].p_id << endl;
-return 0;
+  cout << q[0].p_id << endl;
+  cout << q[1].p_id << endl;
+  return 0;
 }
