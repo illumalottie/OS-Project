182376e2a9aa0de2fb28c78d58d07e3d7d813321
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-13 23:07:12 -0400

    Update adt.h

--- adt.h
+++ adt.h
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <fstream>
 #include <queue>
-#include <stack>
+#include <deque>
 #include <vector>
 using namespace std;
 
@@ -23,12 +23,26 @@ struct Queue {
 }; 
   
 
-// Coder: J Yong + Lottie Maynard
+// Coder(s): J Yong 
+//           Lottie Maynard
 // Purpose: reads in all input from file stream into a struct
 // Pre: Queue struct to read file input in
 //      fileName to open file
 // Post: all input form file has been entered into a queue struct
 //       and an the number of processes has been returned
-int read_File (queue<Queue>& q, string fileName);
+int read_File (deque<Queue>& q, string fileName);
 
+// Coder: J Yong
+// Purpose: eval and inputs new Queues according to their arrival time
+// Pre: deque<Queue> to put the new process in
+//      newProcess - a Queue struct that has not been pushed into the q
+// Post: newProcess is in the deque<Queue> q
+void sortingHat(deque<Queue>& q, Queue newProcess);
+
+// Coder: J Yong
+// Purpose: print the queue
+// Pre: deque to print
+// Post: deque has been printed
+// Note: can be deleted
+void printQ(deque<Queue> q);
 #endif
