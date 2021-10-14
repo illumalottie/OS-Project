3984e41c7ffdee9e92c94a56a2459b5e5eeadbab
DanAossey <92216534+DanAossey@users.noreply.github.com>
2021-10-14 16:11:49 -0400

    Update sjf.h

--- sjf.h
+++ sjf.h
@@ -2,9 +2,13 @@
 // Coder:
 // Purpose:
 //
-#ifndef ADT_H_INCLUDED
-#define ADT_H_INCLUDED
+#ifndef SJF_H_INCLUDED
+#define SJF_H_INCLUDED
 
 #include "adt.h"
+
+int sjf_avg_wait(deque <Queue> q, int numberOfProcesses, bool preempt);
+deque<Queue> readyQueueMaker(deque <Queue>& processes, deque <Queue>& readyQueue, int runningTime);
+deque<Queue> shortestTime(deque<Queue> readyQueue);
 
 #endif 
