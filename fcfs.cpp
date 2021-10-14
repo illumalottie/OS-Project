a6bade7795a2565878d59e51328f1fa59f60a6c5
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-13 23:15:24 -0400

    Update fcfs.cpp

--- fcfs.cpp
+++ fcfs.cpp
@@ -4,3 +4,15 @@
 #include "fcfs.h"
 #include "adt.h"
 
+int fcfs_avg_wait (deque<Queue> q, int processCnt) {
+  int totalWait = 0;
+  deque<Queue> gant = q;
+  
+  while (!gant.empty()) {
+    totalWait = totalWait + gant.front().total_CPU_burst;
+    gant.pop_front();
+  }
+
+  int avg_wait = totalWait/processCnt;
+  return avg_wait; 
+}
