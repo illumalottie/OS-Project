f0dceb072d236f592c94a5ca0fbacc30542ede0d
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-14 15:52:35 -0400

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -12,6 +12,7 @@
 //
 //  OUTPUT: average wait time for each process
 //  SOURCES: 
+//  printQ is a version of showdq https://www.geeksforgeeks.org/deque-cpp-stl/
 //  using struct for a queue/deque https://stackoverflow.com/questions/25074741/how-do-i-store-and-get-a-queue-of-structure
 //  Used deque because it's more flexible than queue https://www.cplusplus.com/reference/deque/deque/push_front/
 //  catch error from stoi https://www.cplusplus.com/reference/stdexcept/invalid_argument/
@@ -28,38 +29,71 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  deque<Queue> q; //maybe do the lexical definition he mentioned? thoughts? - lottie
-  string com[100];
+  deque<Queue> q; 
+  string com[100], fileName, type = "FCFS";
   char f;
-  int i = 0;
+  int flagCnt = 0, quanta = 10;
+  bool preemptive = false;
+  bool verbose = false;
   
-  //THIS IS ALL TESTING 
-  while (--argc >= 0) { //IT WORKS 
-    if ((*++argv)[0] == '-') {
-    //  while (f = *++argv[0]) {
-    } 
-    else { //not flag argv's
-      com[i] = argv[0];
-      cout << endl << com[i];
-      if (com[i] == "yum") {
-        cout << endl << "it works! YUM!";
+  while (--argc > 0) {
+    com[flagCnt] = argv[1];
+    cout << com[flagCnt] << endl;
+    ++argv;
+    ++flagCnt;
+  }
+  cout << endl;
+  
+  for (int i = 0; i < flagCnt; i++) {
+    if(com[i] == "--type"){
+      if (did_user_forget(com[i + 1]) == false) {
+	      if (valid_type(com[i + 1]) == true) {
+	        i++;
+	        type = com[i];
+	      }
       }
-      i++;
+      cout << "TYPE " << type << endl;
+    }
+    else if(com[i] == "--preemptive") {
+      preemptive = true;
+      cout << "PRE " << preemptive << endl;
+    }
+    else if(com[i] == "--quanta") {
+      if (did_user_forget(com[i + 1]) == false) {
+	      i++;
+	      try {quanta = stoi(com[i]);}
+        //catches the user trying to convert a not-integer-representing string
+	      catch (exception &err) { //for the exception http://cplusplus.com/forum/beginner/253696/
+	        cerr << "Invalid Quanta: defaulting to 10\n";
+	      }
+      }
+      cout << "QUANTA " << quanta << endl;
+    }
+    else if(com[i] == "--file"){
+      if (did_user_forget(com[i + 1]) == false) {
+	      i++;
+	      fileName = com[i];
+      }
+      cout << "FILE " << fileName << endl;
+    }
+    else if(com[i] == "--verbose"){
+      verbose = true;
+      cout << "VER " << verbose << endl;
+    }
+    else {
+      cout << com[i] << " is INVALID" << endl;
     }
   }
-  //
   
-  int processCnt = read_File(q, "sched.in");
-  cout << endl << "Number of processes: " << processCnt; //debug comment
-  
-  //i can pass in an Queue array and it brings back all the info from the file
-  //below the couts are just tests to make sure it returns the right numbers - lottie
+  int processCnt = read_File(q, fileName);
+  cout << endl << "Number of processes: " <<  processCnt << endl;
 
-  Queue temp = q.front();
-  cout << endl << "\nDEBUG"
-       << endl << temp.p_id 
-       << " " << temp.arrival_time << endl;
+  int fcfs = fcfs_avg_wait(q, processCnt);
+  cout << endl << "Average Wait Time: " << fcfs << endl;
+  
+  cout << endl << "\nDEBUG" << endl;
   
   printQ(q);
   return 0;
+
 }
