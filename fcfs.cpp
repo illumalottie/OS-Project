d2fb5128ac25f9102f9db27f2a61a2fff780e50e
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-15 01:00:57 -0400

    Update fcfs.cpp
    Was calc avg wait wrong

--- fcfs.cpp
+++ fcfs.cpp
@@ -8,11 +8,10 @@ int fcfs_avg_wait (deque<Queue> q, int processCnt) {
   int totalWait = 0;
   deque<Queue> gant = q;
   
-  while (!gant.empty()) {
-    totalWait = totalWait + gant.front().total_CPU_burst;
-    gant.pop_front();
+  for (int i = 1; i < processCnt; i++) {
+    waitTime = waitTime + q[i-1].total_CPU_burst;
+    totalWait = totalWait + waitTime;
   }
-
   int avg_wait = totalWait/processCnt;
   return avg_wait; 
 }
