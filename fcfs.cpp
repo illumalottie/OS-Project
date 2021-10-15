586d69b47480cbf643329320fee89b3c7d8b25cf
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-15 19:19:37 -0400

    Update fcfs.cpp

--- fcfs.cpp
+++ fcfs.cpp
@@ -4,11 +4,11 @@
 #include "fcfs.h"
 #include "adt.h"
 
-int fcfs_avg_wait (deque<Queue>& q, int processCnt) {
+int fcfs_avg_wait (deque<Queue>& q) {
   int time = 0;
   int totalWait;
   
-  for (int i = 1; i < processCnt; i++) {
+  for (int i = 1; i < q.size(); i++) {
     time = time + q[i-1].total_CPU_burst;
     q[i].waitTime  = time; //changes waitTime in the original deque
     totalWait = totalWait + q[i].waitTime;
