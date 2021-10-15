8c87b0b7799543a9942f65280351279eed7d6ba4
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-15 19:19:30 -0400

    Update fcfs.h

--- fcfs.h
+++ fcfs.h
@@ -10,8 +10,7 @@
 // Coder: J Yong
 // Purpose: finds the avg wait time for fcfs
 // Pre: q is where the total wait time is being pulled from
-//      processCnt is what the total is being divided by
 // Post: fcfs average wait time
-int fcfs_avg_wait(deque<Queue> q, int processCnt);
+int fcfs_avg_wait(deque<Queue> q);
 
 #endif
