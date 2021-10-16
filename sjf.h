6cbd5365f90cb2e707688118a747787456e6ba9c
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-15 20:21:49 -0400

    Update sjf.h

--- sjf.h
+++ sjf.h
@@ -7,9 +7,9 @@
 
 #include "adt.h"
 
-int sjf_avg_wait(deque <Queue> q, int numberOfProcesses, bool preempt, bool verbose);
-int sjf_avg_wait_noPre(deque <Queue> q, int numberOfProcesses, bool verbose);
-int sjf_avg_wait_pre(deque <Queue> q, int numberOfProcesses, bool verbose);
+int sjf_avg_wait(deque <Queue> q, bool preempt, bool verbose);
+int sjf_avg_wait_noPre(deque <Queue> q, bool verbose);
+int sjf_avg_wait_pre(deque <Queue> q, bool verbose);
 deque<Queue> readyQueueMaker(deque <Queue> processes, deque <Queue>& readyQueue, int runningTime);
 deque<Queue> shortestTime(deque<Queue> readyQueue);
 
