02b59bee4b828d7d02e9521ad5b18390e3eb30f2
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-15 21:04:33 -0400

    comments done

--- roundRobin.h
+++ roundRobin.h
@@ -12,7 +12,7 @@
 // Pre: queue are the processes, quanta is quanta, and verbose to see if verbose needs to be printed or not
 // Post: average wait time
 int rr_avg_wait(deque <Queue> q, bool verbose, int quanta);
-
+ 
 // Coder: Dan
 // Purpose: updates what processes are ready based on running time
 // Pre: processes are the original list of processes, queue of ready processes to update, running time to know how to update it
