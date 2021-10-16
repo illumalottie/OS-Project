d5a95d5dbba0c6040126eb634f5f378991bfea2b
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-15 21:09:14 -0400

    comments done

--- priority.h
+++ priority.h
@@ -7,11 +7,23 @@
 
 #include "adt.h"
 
-// Coder(s): 
-// Purpose: 
-// Pre: 
-// Post: 
+// Coder(s): Lottie and Dan
+// Purpose: driver for priority
+// Pre: the queue are the processes to calculate waitTime from and preempt and verbose to check if they need to be done
+// Post: returns the average wait time
 int priority_avg_wait(deque<Queue> q, int numberOfProcessses, bool preempt, bool verbose);
+
+// Coders: Lottie and Dan
+// Purpose: non-preemptive priority algorithm
+// Pre: q are the processes and verbose to see if it needs to be printed
+// Post: average wait time is calc and returned
+int priority_avg_wait_noPre(deque<Queue> q, int numberOfProcessses, bool verbose);
+
+// Coders: Lottie and Dan
+// Purpose: non-preemptive priority algorithm
+// Pre: q are the processes and verbose to see if it needs to be printed
+// Post: average wait time is calc and returned
+int priority_avg_wait_pre(deque<Queue> q, int numberOfProcessses, bool verbose);
 
 // Coder(s): Dan
 // Purpose: updates what processes are ready based on running time
