42312ca825ac903a959a51968ff0ca779f07da88
YomNom <84593177+YomNom@users.noreply.github.com>
2021-10-15 21:04:14 -0400

    Update roundRobin.h

--- roundRobin.h
+++ roundRobin.h
@@ -7,7 +7,16 @@
 
 #include "adt.h"
 
+// Coder: Lottie
+// Purpose: finds the average wait time using the round robin algorithm
+// Pre: queue are the processes, quanta is quanta, and verbose to see if verbose needs to be printed or not
+// Post: average wait time
 int rr_avg_wait(deque <Queue> q, bool verbose, int quanta);
+
+// Coder: Dan
+// Purpose: updates what processes are ready based on running time
+// Pre: processes are the original list of processes, queue of ready processes to update, running time to know how to update it
+// Post: returns updated ready Queue
 deque<Queue> readyQueueMakerRR(deque <Queue> processes, deque <Queue>& readyQueue, int runningTime);
 
 #endif 
